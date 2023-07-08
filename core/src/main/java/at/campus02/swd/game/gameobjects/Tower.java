93f1a80a8e9c3c0cf7d05e93a0ab9b7c6ec35cb3
dfriedl <s51729@edu.campus02.at>
2023-07-08 17:25:23 +0200

    first commit

---
+++ core/src/main/java/at/campus02/swd/game/gameobjects/Tower.java
@@ -0,0 +1,31 @@
+package at.campus02.swd.game.gameobjects;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Tower implements GameObject {
+    private Texture image;
+    private Sprite sprite;
+
+
+    public Tower(){
+
+        image = new Texture("sprites/mapTile_099.png");
+        sprite = new Sprite(image);
+    }
+    @Override
+    public void act(float delta) {
+
+    }
+
+    @Override
+    public void setPosition(float x, float y) {
+        sprite.setPosition(x, y);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+    }
+}
